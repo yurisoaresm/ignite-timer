03b1cb5b3e3dc870f17dcda01422c490caa6292b
Yuri Soares <yurisoaresm@outlook.com>
2023-02-13 14:34:36 -0300

    feat: create context between routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from 'styled-components'
 import { BrowserRouter } from 'react-router-dom'
 
 import { Router } from './Router'
+import { CyclesContextProvider } from './contexts/CyclesContext'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
 
@@ -9,7 +10,9 @@ export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-        <Router />
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>
         <GlobalStyle />
       </BrowserRouter>
     </ThemeProvider>
